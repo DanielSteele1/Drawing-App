5f2c47936863156f7d61bfb4284c40862da4b292
Daniel Steele <Dsteele1906@gmail.com>
2024-07-27 14:59:36 +0100

    worked on menu css and JS

--- src/menu.js
+++ src/menu.js
@@ -6,21 +6,46 @@ function Menu() {
     return (
 
         <div className="menu">
+
+            <div className="menu-item">
+                Choose a brush color:
+                <br></br>
+                <br></br>
+
+                <input type="color" id="color-picker" /> </div>
+
             <div className="menu-items">
 
-                <div id="clear-square">Clear </div>
+                <div className="menu-item">
+                    Line Width
+                    <br></br>
+                    <input type="range" id="line-width" min="1" max="50" />
 
-                <div id="color-square">color </div>
+                </div>
 
-                <div id="size-square">size </div>
+                <div className="menu-item">
+                    Line Shape
+                    <br></br>
+                    <input type="range" id="line-width" min="1" max="50" />
 
-                <div id="shape-square">Shape </div>
+                </div>
 
-                <div id="clear-square">Clear </div>
+                <div className="menu-item">
+                    ???
+                    <br></br>
+                    <input type="range" id="line-width" min="1" max="50" />
 
-                <div id="clear-square">Clear </div>
+                </div>
+
 
             </div>
+
+            <div className="menu-options-container">
+                <div className="menu-options"> Clear </div>
+                <div className="menu-options">Reset Settings</div>
+                <div className="menu-options">Save</div>
+            </div>
+
         </div>
     );
 }
